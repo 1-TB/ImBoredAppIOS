c61efe6162bd43dc0273261dfea75c5cb4a2c480
1-TB <jordan@nsa.icu>
2024-04-22 09:51:58 -0500

    Initial Commit

---
+++ ImBored/ImBoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImBoredApp.swift
+//  ImBored
+//
+//  Created by Jordan Carter on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ImBoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
