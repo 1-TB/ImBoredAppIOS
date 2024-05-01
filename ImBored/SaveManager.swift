420ee8845191dc0cef8c2c4a5aa172552954e271
1-TB <jordan@nsa.icu>
2024-05-01 08:32:39 -0500

    Enhanced SaveManager with load and save functionality
    Significant changes include the addition of load and save functions in the SaveManager class. These functions use JSONEncoder and JSONDecoder to persistently store activities. The addActivity and removeActivity methods now call the save function, ensuring data persistence whenever an activity is added or removed. An initializer was also added to load saved activities upon instantiation of a SaveManager object.

--- ImBored/SaveManager.swift
+++ ImBored/SaveManager.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jordan Carter on 4/29/24.
 //
-
 import Foundation
 
 class SaveManager: ObservableObject {
@@ -12,11 +11,33 @@ class SaveManager: ObservableObject {
     
     func addActivity(_ activity: Activity) {
         savedActivities.append(activity)
+        save()
     }
     
     func removeActivity(_ activity: Activity) {
         if let index = savedActivities.firstIndex(where: { $0.key == activity.key }) {
             savedActivities.remove(at: index)
+            save()
+        }
+    }
+    
+    init() {
+        load()
+    }
+    
+    func save() {
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(savedActivities) {
+            UserDefaults.standard.set(encodedData, forKey: "savedActivities")
+        }
+    }
+    
+    func load() {
+        if let savedData = UserDefaults.standard.data(forKey: "savedActivities") {
+            let decoder = JSONDecoder()
+            if let decodedActivities = try? decoder.decode([Activity].self, from: savedData) {
+                savedActivities = decodedActivities
+            }
         }
     }
 }
